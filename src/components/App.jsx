fc8f26308003d23cb93b8bf29d85d3cf3247a8d4
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-10-05 20:53:18 +0300

    update class to hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,26 +5,25 @@ import { ImageGallery } from './ImageGallery/ImageGallery';
 import { pixabaySerch } from './PixabaySerch/PixabaySerch';
 import Loader from './Loader/Loader';
 import { ButtonLoadMore } from './Button/Button';
+import { useEffect, useState } from 'react';
 
-export class App extends Component {
-  state = {
-    searchItem: '',
-    page: 1,
-    total: 1,
-    items: [],
-    isLoading: false,
-    isError: false,
-  };
+export const App = () => {
+  const [searchItem, setSearchItem] = useState('');
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(1);
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  async componentDidUpdate(_, prevState) {
-    const { searchItem, page } = this.state;
-    if (prevState.searchItem !== searchItem || prevState.page !== page) {
-      this.setState({
-        isLoading: true,
-      });
-      try {
-        const result = await pixabaySerch(searchItem, page);
-        const { totalHits, hits } = result;
+  useEffect(() => {
+    if (searchItem === '') {
+      return;
+    }
+
+    setIsLoading(true);
+
+    pixabaySerch(searchItem, page)
+      .then(({ totalHits, hits }) => {
         const onlyNeedValues = hits.map(
           ({ id, tags, webformatURL, largeImageURL }) => ({
             id,
@@ -38,50 +36,48 @@ export class App extends Component {
           toast.warn(
             'Sorry, nothing was found for your request, try something else'
           );
-          return this.setState({
-            isLoading: false,
-            isError: true,
-          });
+
+          setIsLoading(false);
+          setIsError(true);
+          return;
         }
         if (page === 1 && hits.length > 1) {
           toast.success('Hooray! We found what you were looking for');
         }
-        this.setState(prevState => ({
-          items: [...prevState.items, ...onlyNeedValues],
-          isLoading: false,
-          isError: false,
-          total: hits.length,
-        }));
-      } catch (error) {
-        toast.error('Oops, something went wrong, please try again');
-      }
-    }
-  }
 
-  handleFormSubmit = searchItem => {
-    if (this.state.searchItem === searchItem.trim()) {
+        setItems(prevState => [...prevState, ...onlyNeedValues]);
+        setIsLoading(false);
+        setIsError(false);
+        setTotal(hits.length);
+      })
+      .catch(isError => {
+        toast.error('Oops, something went wrong, please try again');
+        setIsError(isError.message);
+        setIsLoading(false);
+      });
+  }, [searchItem, page]);
+
+  const handleFormSubmit = search => {
+    if (searchItem === search.trim()) {
       return toast.info(
         'It looks like there are already pictures found for your request, please check if this wil be a new search'
       );
     }
-    this.setState({ searchItem, items: [], page: 1, isLoading: false });
+    setSearchItem(search);
+    setItems([]);
+    setPage(1);
+    setIsLoading(false);
   };
+  const clickLoadMoreBtn = () => setPage(prevState => prevState + 1);
 
-  ClickLoadMoreBtn = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
+  return (
+    <div>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {isLoading ? <Loader /> : <ImageGallery allItems={items} />}
+      {isError && <p>'Oops, something went wrong! Please, try again'</p>}
+      {total >= 12 && <ButtonLoadMore onClick={clickLoadMoreBtn} />}
+      <ToastContainer autoClose={3000} />
+    </div>
+  );
+};
 
-  render() {
-    const { items, isLoading, total } = this.state;
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {isLoading ? <Loader /> : <ImageGallery allItems={items} />}
-        {total >= 12 && <ButtonLoadMore onClick={this.ClickLoadMoreBtn} />}
-        <ToastContainer autoClose={3000} />
-      </div>
-    );
-  }
-}
